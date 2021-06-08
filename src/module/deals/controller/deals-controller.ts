15d19276dac847341aa1c21d3951cc600ae49bc0
Luiz Felipe Limao <limao@limao.com>
2021-06-07 21:48:13 -0300

    feat: deals query params

--- src/module/deals/controller/deals-controller.ts
+++ src/module/deals/controller/deals-controller.ts
@@ -1,12 +1,18 @@
 import { Context } from 'https://deno.land/x/oak@v7.5.0/context.ts'
 import { ok } from '../../../helpers/oak-http-helper.ts'
 
-export class SalesController {
+export class DealsController {
 
     handle(context: Context){
+
+        const params = context.request.url.searchParams
+
+        const price = params.get('price')
+        const orderby = params.get('orderby')
         
         const _obj = {
-            message:'Hello Sales'
+            price,
+            orderby
         }
             
         return ok(_obj, context)
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import { Application } from 'https://deno.land/x/oak@v7.5.0/mod.ts'
 
-import salesRouter from './module/sales/route/sales-router.ts'
+import dealsRouter from './module/deals/route/deals-router.ts'
 import gamesRouter from './module/games/route/games-router.ts'
 
 const app = new Application();
 
-app.use(salesRouter.routes())
+app.use(dealsRouter.routes())
 app.use(gamesRouter.routes())
 
 const hostname = '0.0.0.0';
